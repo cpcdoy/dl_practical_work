4be783b948f90ea2417ca480bdd88da129395e20
cpcdoy <cpcdoy@gmail.com>
2024-05-24 14:27:40 +0200

    Add: tp-2 base code

---
+++ practical_work_2_cnn/model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class Net(nn.Module):
+    def __init__(self, upscale_factor):
+        super(Net, self).__init__()
+
+        # DESIGN YOUR MODEL ARCHITECTURE HERE
+
+        pass
+
+    def forward(self, x):
+        # RUN YOUR MODEL ARCHITECTURE HERE
+
+        pass
